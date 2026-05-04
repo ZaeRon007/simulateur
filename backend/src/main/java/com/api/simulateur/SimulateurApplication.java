325fac328bec14b4d12e5f7911f791ebbbef1213
ZaeRon007 <julien.pelissier8@orange.fr>
2026-05-04 13:07:36 +0200

    initial files for back-end

---
+++ backend/src/main/java/com/api/simulateur/SimulateurApplication.java
@@ -0,0 +1,13 @@
+package com.api.simulateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulateurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulateurApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/api/simulateur/SimulateurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.simulateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulateurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
